a09818643451e6d52eddefda002514194cc92312
alceballosa <alceballosa@unal.edu.co>
2021-06-10 13:55:24 -0500

    Clase 2 Sem 4 finalizada

--- Semana_4_2y3/vec_alta_prec/claseVecAP.py
+++ Semana_4_2y3/vec_alta_prec/claseVecAP.py
@@ -13,7 +13,7 @@ class altaPrecision(vector):
         print()
 
     def reiniciar(self):
-        for i in range(1, self.n+1):
+        for i in range(1, self.n + 1):
             self.V[i] = None
         self.V[0] = self.n
 
@@ -23,9 +23,58 @@ class altaPrecision(vector):
             print("Valor muy grande para asignar")
         else:
             self.reiniciar()
-            #indexar una cadena por [::-1] la invierte
             i = -1
             while i >= -len(valor_cadena):
                 self.asignaDato(int(valor_cadena[i]), i)
-                i = i-1
+                i = i - 1
             self.V[0] = self.n - len(valor_cadena)
+
+    # vec1, vec2
+    # vec1+vec2
+
+    def sumaYacarreo(self, a, b=0):
+        global acarreo
+        s = a + b + acarreo
+        if s > 9:
+            acarreo = s // 10
+            s = s - 10
+        else:
+            acarreo = 0
+        return s
+
+    def retornaDato(self, i):
+        if i > self.n:
+            print("Indice fuera del rango de valores llenos del vector")
+        else:
+            return self.V[i]
+
+    def __add__(self, b):
+        global acarreo
+        i = self.tamagno()
+        j = b.tamagno()
+        k = max(i, j) + 2  # tamaño del vector resultado
+        resultado = altaPrecision(k)
+        acarreo = 0
+        while i > self.V[0] and j > b.V[0]:
+            r = self.sumaYacarreo(self.retornaDato(i), b.retornaDato(j))
+            resultado.asignaDato(r, k)
+            i = i - 1
+            j = j - 1
+            k = k - 1
+            #resultado.imprimeVector()
+            print("-----------")
+        while i > self.V[0]:
+            r = self.sumaYacarreo(self.retornaDato(i), 0)
+            resultado.asignaDato(r, k)
+            i = i - 1
+            k = k - 1
+        while j > b.V[0]:
+            r = self.sumaYacarreo(b.retornaDato(j))
+            resultado.asignaDato(r, k)
+            j = j - 1
+            k = k - 1
+        if acarreo > 0:
+            resultado.asignaDato(acarreo, k)
+            k = k - 1
+        resultado.V[0] = k
+        return resultado
--- Semana_4_2y3/vec_alta_prec/ejemplos.py
+++ Semana_4_2y3/vec_alta_prec/ejemplos.py
@@ -1,1 +1,13 @@
 from claseVecAP import altaPrecision
+
+
+vap1 = altaPrecision(n=50)
+vap1.asignaValor("2000")
+vap1.imprimeVector()
+
+vap2 = altaPrecision(n=50)
+vap2.asignaValor("100")
+vap2.imprimeVector()
+
+vap3 = vap1 + vap2
+vap3.imprimeVector()
