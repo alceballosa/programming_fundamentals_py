598685d406bdedca9f6afa2276444d5a5b42b1ba
alceballosa <alceballosa@unal.edu.co>
2021-06-26 02:06:33 -0500

    Tkinter test

---
+++ Semana6_3/ejemplos1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+# Tutorial en español: https://python-para-impacientes.blogspot.com/p/tutorial-de-tkinter.html
+# https://www.adictosaltrabajo.com/2020/06/30/interfaces-graficas-en-python-con-tkinter/
+# https://docs.python.org/es/3/library/tk.html
+
+
+print(tk)
